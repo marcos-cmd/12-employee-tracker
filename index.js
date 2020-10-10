53ac036f214ebc694938a468aea3e15ed8fac0dc
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-10-10 13:12:08 -0700

    updated addEmployee func

--- index.js
+++ index.js
@@ -78,5 +78,76 @@ const addRole = () => {
         viewRole();
     })
 }
+const addEmployee = () => {
+    connection.query('SELECT * FROM roles;', (err, res) => {
+        if (err) throw err;
+        let rolesArr = [];
+        for (let i=0; i < res.length; i++) {
+            rolesArr.push(res[i]);
+        }
+        inquirer.prompt([
+            {
+                name: 'first',
+                type: 'input',
+                message: "Enter the employee's first name"
+            },
+            {
+                name: 'last',
+                type: 'input',
+                message: "Enter the employee's last name"
+            },
+            {
+                name: 'role',
+                type: 'list', 
+                message: "Enter the title of this employee's role"
+                choices: function() {
+                    return res.map(res => res.title);
+                }
+            }
+        ]).then((res) => {
+            let first_name = res.first;
+            let last_name = res.last;
+            let role_id;
+            for (let i = 0; i < rolesArr.length; i++) {
+                if (res.role === rolesArr[i].title) {
+                    role_id = rolesArr[i].id;
+                }
+            }
+            connection.query('SELECT * FROM employees ;', (err, res) => {
+                if (err) throw err; 
+                let employeeArr = [];
+                for (let i = 0; i < res.length; i++) {
+                    employeeArr.push(res[i]);
+                }
+                inquirer.prompt({
+                    name: 'manager',
+                    type: 'list',
+                    message: "Select this employee's manager",
+                    choices: function() {
+                        let arr = ['None']
+                        if (res.length > 0) {
+                            for (let i = 0; i < employeeArr.length; i++) {
+                                arr.push(`${employeeArr[i].first_name} ${employeeArr[i].last_name}`);
+                            }
+                        }
+                        return arr;
+                    }
+                }).then(res => {
+                    let manager_id;
+                    for (let i =0; i < employeeArr.length; i++) {
+                        if (res.manager === `${employeeArr[i].first_name} ${employeeArr[i].last_name}`) {
+                            manager_id = employeeArr[i].id;
+                        }
+                    }
+                    const query = 'INSERT INTO employees Set ?;';
+                    connection.query(query, {first_name, last_name, role_id, manager_id}, err => {
+                        if (err) throw err;
+                    });
+                    viewEmployee();
+                });
+            });
+        })
+    })
+}
 
 start();
